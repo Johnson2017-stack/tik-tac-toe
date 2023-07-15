bd49a9304aa73c92bd9501fa99ec4bb404944401
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-07-15 14:51:31 -0700

    added styles for better UX and UI as well as the function to render the move and to be able to go back a move

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setsquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
+ 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i] ) {
       return;
     }
     const nextSquares = squares.slice();
@@ -22,8 +20,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setsquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -55,6 +52,48 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+export default function Game() {
+  const[history, setHistory] = useState([Array(9).fill(null)]);
+  const[currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol className="move">{moves}</ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares) {
